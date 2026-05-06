d8366a6d4595265df709515f289f61b4c10e8c9a
Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>
2026-05-06 15:23:38 +0530

    Fix project explorer loading issue

--- workspaces/mi/mi-extension/src/constants/index.ts
+++ workspaces/mi/mi-extension/src/constants/index.ts
@@ -209,6 +209,5 @@ export const ERROR_MESSAGES = {
 };
 
 export const WI_EXTENSION_ID = 'wso2.wso2-integrator';
-export const WI_PROJECT_EXPLORER_VIEW_ID = 'wso2-integrator.explorer';
 export const MI_PROJECT_EXPLORER_VIEW_ID = 'MI.project-explorer';
 export const MI_RUNTIME_SERVICES_PANEL_ID = 'micro-integrator.runtime-services-panel';
