ae8c69074708279300bfc3c1a8444bdf981bae5f
ChinthakaJ98 <chinthakajayatilake98@gmail.com>
2026-08-31 18:47:49 +0530

    Fix trivy detected vulnerabilities

--- common/config/rush/.pnpmfile.cjs
+++ common/config/rush/.pnpmfile.cjs
@@ -22,7 +22,7 @@ module.exports = {
         if (deps['xmldom']) deps['xmldom'] = 'npm:@xmldom/xmldom@0.8.10';
         if (deps['braces']) deps['braces'] = '3.0.3';
         if (deps['micromatch']) deps['micromatch'] = '4.0.8';
-        if (deps['js-yaml']) deps['js-yaml'] = '4.3.0';
+        if (deps['js-yaml']) deps['js-yaml'] = '4.3.1';
         if (deps['diff']) deps['diff'] = '8.0.3';
         if (deps['eslint']) deps['eslint'] = '^9.27.0';
         if (deps['fast-xml-parser']) deps['fast-xml-parser'] = '5.7.0';
@@ -32,7 +32,7 @@ module.exports = {
         if (deps['hono']) deps['hono'] = '4.12.18'; // CVE-2026-44455 (JSX injection), CVE-2026-44456 (bodyLimit bypass)
         if (deps['@hono/node-server']) deps['@hono/node-server'] = '1.19.13';
         if (deps['@tootallnate/once']) deps['@tootallnate/once'] = '3.0.1';
-        if (deps['dompurify']) deps['dompurify'] = '3.4.12'; // security fixes
+        if (deps['dompurify']) deps['dompurify'] = '3.4.13'; // security fixes
         if (deps['axios']) deps['axios'] = '1.18.0'; // security fixes
         if (deps['body-parser']) deps['body-parser'] = '1.20.6';
         if (deps['http-proxy-middleware']) deps['http-proxy-middleware'] = '3.0.7'; // security fixes
@@ -49,12 +49,13 @@ module.exports = {
         if (deps['flatted']) deps['flatted'] = '3.4.2'; // security fix
         if (deps['handlebars']) deps['handlebars'] = '4.7.9'; // security fix: prototype pollution
         if (deps['tmp']) deps['tmp'] = '0.2.7'; // security fix
-        if (deps['undici']) deps['undici'] = '7.28.0'; // security fixes
+        if (deps['undici']) deps['undici'] = '7.29.0'; // security fixes
         if (deps['@opentelemetry/core']) deps['@opentelemetry/core'] = '2.8.0'; // security fix
         if (deps['@opentelemetry/propagator-jaeger']) deps['@opentelemetry/propagator-jaeger'] = '2.9.0';
         if (deps['uuid']) deps['uuid'] = '14.0.0'; // security fix
         if (deps['@nevware21/ts-utils']) deps['@nevware21/ts-utils'] = '0.14.0'; // security fix: CVE-2026-46681 (prototype pollution)
         if (deps['webpack-dev-server']) deps['webpack-dev-server'] = '5.2.6'; // security fix
+        if (deps['fast-uri']) deps['fast-uri'] = '3.1.6';
         if (deps['ws']) {
           if (/^[\s\^~><=]*8[.\s]/.test(deps['ws'])) {
             deps['ws'] = '8.21.0'; // security fix
@@ -121,13 +122,13 @@ module.exports = {
           const currentVersion = deps['brace-expansion'];
           let newVersion;
           if (currentVersion.startsWith('^1') || currentVersion.startsWith('1')) {
-            newVersion = '1.1.17';
+            newVersion = '1.1.18';
           } else if (currentVersion.startsWith('^2') || currentVersion.startsWith('2')) {
-            newVersion = '2.1.3';
+            newVersion = '2.1.4';
           } else if (currentVersion.startsWith('^3') || currentVersion.startsWith('3')) {
-            newVersion = '3.0.5';
+            newVersion = '3.0.6';
           } else if (currentVersion.startsWith('^5') || currentVersion.startsWith('5')) {
-            newVersion = '5.0.8'; // security fix: CVE-2026-45149 (DoS numeric range)
+            newVersion = '5.0.9';
           } else {
             context.log(`Unexpected brace-expansion version: ${currentVersion}`);
             newVersion = currentVersion;
